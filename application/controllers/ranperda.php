0554c9d7ea190722706d8132b5112f99a20e1e9c
wahyu.ardie <wahyu.ardie@cp.co.id>
2021-07-14 19:03:44 +0700

    message error 1

--- application/controllers/ranperda.php
+++ application/controllers/ranperda.php
@@ -572,7 +572,7 @@ class Ranperda extends CI_Controller {
 				"curst"=> $this->input->post("curst"),
 				"group_user" => $this->session->userdata('group_user'),
 				"iscls"=> '',
-				"notif" => $this->Global_model->getNotif(1,"Ranperda hanya bisa diinput 1 hari sekali")
+				"notif" => $this->Global_model->getNotif(1,"Penyampaian Pengajuan Dokumen Evaluasi Rancangan Perda dapat dilakukan 1 kali dalam 1 hari, selanjutnya pengajuan berikut dapat dilakukan 24 jam kemudian. Terima Kasih")
 			);
 			echo json_encode($ranperdaObj);
 			exit;
